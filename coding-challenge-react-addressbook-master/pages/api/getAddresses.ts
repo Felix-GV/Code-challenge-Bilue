ddb3272cec9f972a5991354a3bb0f6e4ea1dc20c
Felix-GV <felixgehrtverney@gmail.com>
2023-02-18 15:33:47 +1100

    Bonus challenge refactor

--- coding-challenge-react-addressbook-master/pages/api/getAddresses.ts
+++ coding-challenge-react-addressbook-master/pages/api/getAddresses.ts
@@ -1,62 +1,48 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
 import generateMockAddresses from "../../src/utils/generateMockAddresses";
+import { NextApiRequest, NextApiResponse } from "next";
+
+function validateInput(input: string, name: string) {
+  if (!input) {
+    throw new Error(`${name} field is mandatory!`);
+  }
+
+  // !/^\d+$/ checks that there are only digits
+  if (!/^\d+$/.test(input)) {
+    throw new Error(`${name} must be all digits!`);
+  }
+
+  return input;
+}
 
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const {
-    query: { postcode, streetnumber },
-  } = req;
+  const { postcode, streetnumber } = req.query;
 
-  if (!postcode || !streetnumber) {
+  try {
+    const validatedPostcode = validateInput(postcode as string, "Postcode");
+    const validatedStreetNumber = validateInput(
+      streetnumber as string,
+      "Street number"
+    );
+
+    const mockAddresses = generateMockAddresses(
+      validatedPostcode,
+      validatedStreetNumber
+    );
+    if (mockAddresses) {
+      return res.status(200).json({
+        status: "ok",
+        details: mockAddresses,
+      });
+    }
+
+    throw new Error("No results found!");
+  } catch (error) {
     return res.status(400).send({
       status: "error",
-      errormessage: "Postcode and street number fields mandatory!",
+      throw: error,
     });
   }
-
-  if (postcode.length < 4) {
-    return res.status(400).send({
-      status: "error",
-      errormessage: "Postcode must be at least 4 digits!",
-    });
-  }
-
-  /** TODO: Refactor the code below so there is no duplication of logic for postCode/streetNumber digit checks. */
-
-  const postCode = parseInt(postcode as string);
-
-  if (isNaN(postCode)) {
-    return res.status(400).send({
-      status: "error",
-      errormessage: "Postcode must be all digits!",
-    });
-  }
-
-  const streetNumber = parseInt(streetnumber as string);
-
-  if (isNaN(streetNumber)) {
-    return res.status(400).send({
-      status: "error",
-      errormessage: "Street number must be all digits!",
-    });
-  }
-
-  const mockAddresses = generateMockAddresses(
-    postcode as string,
-    streetnumber as string
-  );
-  if (mockAddresses) {
-    return res.status(200).json({
-      status: "ok",
-      details: mockAddresses,
-    });
-  }
-
-  return res.status(404).json({
-    status: "error",
-    errormessage: "No results found!",
-  });
 }
